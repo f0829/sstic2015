ed7f6e496a92422bdbe6c83d948f696a63f2a80b
Julien Perrot <jperrot@cossi.internet>
2015-04-17 17:10:28 +0200

    improve clean.js

--- stage-4/clean.js
+++ stage-4/clean.js
@@ -1,4 +1,5 @@
 var fs = require('fs');
+
 var esprima = require('esprima');
 var estraverse = require('estraverse');
 var escodegen = require('escodegen');
@@ -10,16 +11,15 @@ var ast = esprima.parse(fs.readFileSync(filename));
 function simplify_node(node) {
     if ((node.type == 'BinaryExpression') &&
 	    (node.left.type == 'Literal') &&
-	    (node.right.type == 'Literal'))
-    {
-	if (node.operator == '+') {
-	    return { type: 'Literal', value: node.left.value + node.right.value,
-		raw: node.left.raw + node.right.raw };
-	} else if (node.operator == '*') {
-	    return { type: 'Literal', value: node.left.value * node.right.value,
-		raw: node.left.raw * node.right.raw };
-	}
-    }
+	    (node.right.type == 'Literal')) {
+		if (node.operator == '+') {
+		    return { type: 'Literal', value: node.left.value + node.right.value,
+			raw: node.left.raw + node.right.raw };
+		} else if (node.operator == '*') {
+		    return { type: 'Literal', value: node.left.value * node.right.value,
+			raw: node.left.raw * node.right.raw };
+		}
+	    }
     return node;
 }
 
@@ -27,60 +27,94 @@ function simplify_ast(ast) {
     var assignments = {};
     var renaming = {};
     var f_count = 0;
-    var arg_count = 0;
+
+    var scopeChain = [];
 
     estraverse.traverse(ast, {
 	enter: function(node, parent) {
+	    var scopeName;
+	    if (node.type == 'FunctionDeclaration')
+	    	scopeName = 'function_' + node.id.name;
+	 
+	    if (node.type == 'Program') {
+	    	scopeName = 'program';
+	    }
+	    if (scopeName) {
+	    	scopeChain.push(scopeName);
+	    	assignments[scopeName] = {};
+	    }
+
 	    if (node.type == 'Identifier') {
-		k = assignments[node.name];
+	    	var currentScope = scopeChain[scopeChain.length - 1];
+		k = assignments[currentScope][node.name];
 		if (parent.type == 'AssignmentExpression') {
 		    /* Node is on the left-hand side of the assignment */
 		    if (parent.left == node) {
 			right = parent.right;
-			if ( (right.type == 'Literal')
-			    || (right.type == 'Identifier')
-			    // || (right.type == 'MemberExpression') 
-			   ) {
-			       if (!k) {
-				   assignments[node.name] = { node: right, ref_count: 1 };
-			       } else {
-				   /* Not the first assignment, increasing ref count */
-				   k['ref_count'] += 1;
-				   assignments[node.name] = k;
-			       }
-			   } 
+			    if (!k) {
+				assignments[currentScope][node.name] = { node: right, ref_count: 1 };
+			    } else {
+				/* Not the first assignment, increasing ref count */
+				k['ref_count']++;
+			    }
 		    }
 		} else if (parent.type == 'UpdateExpression') {
 		    if (k) {
-			k['ref_count'] += 1;
-			assignments[node.name] = k;
+			k['ref_count']++;
 		    }
-		} else if ((parent.type == 'FunctionDeclaration') && (parent.id == node)) {
+		} /*else if ((parent.type == 'FunctionDeclaration') && (parent.id == node)) {
 		    renaming[node.name] = "func_" + f_count;
-		    f_count += 1;
-		} 
+		    f_count++;
+		} */
 	    }
-	}
+	},
+	    leave: function(node, parent) {
+	    	if ((node.type == 'FunctionDeclaration') || (node.type == 'Program')) {
+	    	    scopeChain.pop();
+		}
+	    }
     });
+
+    scopeChain = [];
 
     result = estraverse.replace(ast, {
 	enter: function(node, parent) {
-	    if (node.type == 'Identifier') {
-		k = assignments[node.name];
-		if ( (parent.type == 'AssignmentExpression') && (parent.left == node) ||
-		    (parent.type == 'UpdateExpression')
-		   ) {
-		       return node;
-		   }
-		if (k && k['ref_count'] == 1) {
-		    return k.node;
+	    if (node.type == 'FunctionDeclaration') {
+	    	scopeChain.push('function_' + node.id.name);
+	    }
+	    if (node.type == 'Program') {
+	    	scopeChain.push('program');
+	    }
+
+	    if (node.type == 'Identifier') {
+	    	var currentScope = scopeChain[scopeChain.length - 1];
+		k = assignments[currentScope][node.name];
+		if (!k) {
+	    	    console.log("var not found, scope: " + currentScope + ", var: " + node.name);
+		    k = assignments['program'][node.name];
 		}
+		if ( (parent.type == 'AssignmentExpression') && (parent.left == node) ||
+		    (parent.type == 'UpdateExpression') ) {
+			return node;
+		    }
+		if (k && k['ref_count'] == 1) {
+		    if ((k.node.type == 'Literal') || (k.node.type == 'Identifier')) {
+		    	return k.node;
+		    }
+		}
+		/*
 		if (renaming[node.name]) {
 		    node.name = renaming[node.name];
 		}
+		*/
 	    }
 	    return simplify_node(node);
-	}
+	},
+	    leave: function(node, parent) {
+	    	if ((node.type == 'FunctionDeclaration') || (node.type == 'Program')) {
+	    	    scopeChain.pop();
+		}
+	    }
     });
 
     return result;
